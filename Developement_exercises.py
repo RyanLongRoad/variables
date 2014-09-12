1b3f72bf65296921edba89192311cbd1218766cc
RyanLongRoad <4327@longroad.ac.uk>
2014-09-12 16:05:34 +0100

    Development_exercises

---
+++ Developement_exercises.py
@@ -0,0 +1,13 @@
+#Ryan Cox
+#12/09/2014
+#Development exercises
+
+import math
+
+number1 = int(input("Please enter number 1: "))
+
+number2 = int(input("Please enter number 2: "))
+
+answer = float(number1/number2)
+
+print("The answer is {0}.".format(answer))
